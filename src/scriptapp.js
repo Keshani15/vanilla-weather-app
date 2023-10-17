177774060b2057048463b0afaf48c7ea44a79768
Keshani15 <147650428+Keshani15@users.noreply.github.com>
2023-10-18 00:15:25 +0200

    added live data in JS: temperature, City, wind, humidity and description

--- src/scriptapp.js
+++ src/scriptapp.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  descriptionElement.innerHTML = response.data.condition.description;
+}
 
+let apiKey = `a6bb6oe20805b9ecd0dta4d24747d30f`;
+let city = "London";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
