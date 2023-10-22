e4641c9d0b287bf3a2aeabfdfbd50b966c6f88f6
Keshani15 <147650428+Keshani15@users.noreply.github.com>
2023-10-22 13:10:08 +0200

    removed console log

--- src/scriptapp.js
+++ src/scriptapp.js
@@ -33,7 +33,6 @@ function formateDay(timestamp) {
 function displayForecast(response) {
   let forecast = response.data.daily;
 
-  console.log(forecast);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
